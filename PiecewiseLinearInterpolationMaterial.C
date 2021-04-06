793722e1c49c8f81c76cdcc7b740b00a3c736cf6
zhang-rite <79129885+zhang-rite@users.noreply.github.com>
2021-04-06 15:47:01 +0900

    Create PiecewiseLinearInterpolationMaterial.C

---
+++ PiecewiseLinearInterpolationMaterial.C
@@ -0,0 +1,209 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "PiecewiseLinearInterpolationMaterial.h"
+#include "PiecewiseTabularBase.h"
+//#include "Piecewise.h"
+#include "DelimitedFileReader.h"
+
+// MOOSE includes
+#include "MooseVariableFE.h"
+
+registerMooseObject("MooseApp", PiecewiseLinearInterpolationMaterial);
+
+defineLegacyParams(PiecewiseLinearInterpolationMaterial);
+
+InputParameters
+PiecewiseLinearInterpolationMaterial::validParams()
+{
+  
+  InputParameters params = Material::validParams();
+  params.addClassDescription("Compute a property using a piecewise linear interpolation to define "
+                             "its dependence on a variable");
+  params.addRequiredParam<std::string>("property",
+                                       "The name of the property this material will compute");
+  params.addRequiredCoupledVar(
+      "variable",
+      "The name of the variable whose value is used as the abscissa in the interpolation");
+
+
+  params.addParam<std::vector<Real>>("x", "The abscissa values");
+  params.addParam<std::vector<Real>>("y", "The ordinate values");
+  params.addParam<std::vector<Real>>("xy_data",
+                                     "All function data, supplied in abscissa, ordinate pairs");
+  params.addParam<FileName>("data_file", "File holding csv data for use with Piecewise");
+  params.addParam<unsigned int>("x_index_in_file", 0, "The abscissa index in the data file");
+  params.addParam<unsigned int>("y_index_in_file", 1, "The ordinate index in the data file");
+  params.addParam<bool>(
+      "xy_in_file_only", true, "If the data file only contains abscissa and ordinate data");
+
+  MooseEnum format("columns=0 rows=1", "rows");
+  params.addParam<MooseEnum>(
+      "format", format, "Format of csv data file that is in either in columns or rows");
+  params.addParam<Real>("scale_factor", 1.0, "Scale factor to be applied to the ordinate values");
+  params.addParam<bool>(
+      "extrapolation",
+      false,
+      "Use linear extrapolation to evaluate points that lie outside given data set domain. ");
+  params.declareControllable("scale_factor");  
+  return params;
+}
+
+PiecewiseLinearInterpolationMaterial::PiecewiseLinearInterpolationMaterial(
+    const InputParameters & parameters)
+  : DerivativeMaterialInterface<Material>(parameters),
+    _prop_name(getParam<std::string>("property")),
+    _coupled_var(coupledValue("variable")),
+    _scale_factor(getParam<Real>("scale_factor")),
+    _extrap(getParam<bool>("extrapolation")),    
+    _property(declareProperty<Real>(_prop_name)),
+    _dproperty(declarePropertyDerivative<Real>(_prop_name, getVar("variable", 0)->name()))
+{
+  std::pair<std::vector<Real>, std::vector<Real>> xy;
+  std::vector<Real> x;
+  std::vector<Real> y;
+
+  if (isParamValid("data_file"))
+    xy = buildFromFile();
+  else if (isParamValid("x") || isParamValid("y"))
+    xy = buildFromXandY();
+
+  else if (isParamValid("xy_data"))
+    xy = buildFromXY();
+  else
+    mooseError("In Piecewise ",
+               _name,
+               ": Either 'data_file', 'x' and 'y', or 'xy_data' must be specified.");
+  //setData(xy.first, xy.second);
+  try
+  {
+    // _linear_interp = libmesh_make_unique<LinearInterpolation>(xy.first ,xy.second);
+    _linear_interp = libmesh_make_unique<LinearInterpolation>(x, y, _extrap);
+  }
+  catch (std::domain_error & e)
+  {
+    mooseError("In PiecewiseLinearInterpolationMaterial ", _name, ": ", e.what());
+  }  
+}
+
+
+
+
+std::pair<std::vector<Real>, std::vector<Real>>
+PiecewiseLinearInterpolationMaterial::buildFromFile()
+{
+  // Input parameters
+  const FileName & data_file_name = getParam<FileName>("data_file");
+  const MooseEnum & format = getParam<MooseEnum>("format");
+  unsigned int x_index = getParam<unsigned int>("x_index_in_file");
+  unsigned int y_index = getParam<unsigned int>("y_index_in_file");
+  bool xy_only = getParam<bool>("xy_in_file_only");
+
+  // Check that other forms of input are not set.
+  if (isParamValid("x") || isParamValid("y") || isParamValid("xy_data"))
+    mooseError("In Piecewise ",
+               _name,
+               ": Cannot specify 'data_file' and 'x', 'y', or 'xy_data' together.");
+
+  if (x_index == y_index)
+    mooseError("In Piecewise ",
+               _name,
+               ": 'x_index_in_file' and 'y_index_in_file' are set to the same value.");
+
+  // Read the data from CSV file
+  MooseUtils::DelimitedFileReader reader(data_file_name, &_communicator);
+  reader.setFormatFlag(format.getEnum<MooseUtils::DelimitedFileReader::FormatFlag>());
+  reader.setComment("#");
+  reader.read();
+  const std::vector<std::vector<double>> & data = reader.getData();
+
+  // Check the data format
+  if (x_index >= data.size())
+    mooseError("In Piecewise ",
+               _name,
+               ": The 'x_index_in_file' is out-of-range of the available data in '",
+               data_file_name,
+               "', which contains ",
+               data.size(),
+               " ",
+               format,
+               " of data.");
+
+  if (y_index >= data.size())
+    mooseError("In Piecewise ",
+               _name,
+               ": The 'y_index_in_file' is out-of-range of the available data in '",
+               data_file_name,
+               "', which contains ",
+               data.size(),
+               " ",
+               format,
+               " of data.");
+
+  if (data.size() > 2 && xy_only)
+    mooseError("In Piecewise ",
+               _name,
+               ": Read more than two ",
+               format,
+               " of data from file '",
+               data_file_name,
+               "'.  Did you mean to use \"format = ",
+               format == "columns" ? "rows" : "columns",
+               "\" or set \"xy_in_file_only\" to false?");
+
+  // Update the input vectors to contained the desired data
+  return std::make_pair(reader.getData(x_index), reader.getData(y_index));
+}
+
+std::pair<std::vector<Real>, std::vector<Real>>
+PiecewiseLinearInterpolationMaterial::buildFromXandY()
+{
+  if (!isParamValid("x") || !isParamValid("y"))
+    mooseError(
+        "In Piecewise ", _name, ": Both 'x' and 'y' must be specified if either one is specified.");
+
+  if (isParamValid("xy_data"))
+    mooseError("In Piecewise ", _name, ": Cannot specify 'x', 'y', and 'xy_data' together.");
+
+  return std::make_pair(getParam<std::vector<Real>>("x"), getParam<std::vector<Real>>("y"));
+}
+
+std::pair<std::vector<Real>, std::vector<Real>>
+PiecewiseLinearInterpolationMaterial::buildFromXY()
+{
+  std::vector<Real> xy = getParam<std::vector<Real>>("xy_data");
+  unsigned int xy_size = xy.size();
+  if (xy_size % 2 != 0)
+    mooseError(
+        "In Piecewise ", _name, ": Length of data provided in 'xy_data' must be a multiple of 2.");
+
+  unsigned int data_size = xy_size / 2;
+  std::vector<Real> x;
+  std::vector<Real> y;
+  x.reserve(data_size);
+  y.reserve(data_size);
+  // for (unsigned int i = 0; i < xy_size; i += 2)
+  // {
+  //   x.push_back(xy[i]);
+  //   y.push_back(xy[i + 1]);
+  // }
+  for (unsigned int i = 0; i < xy_size / 2; ++i)
+  {
+    x.push_back(xy[i * 2]);
+    y.push_back(xy[i * 2 + 1]);
+  }
+  return std::make_pair(x, y);
+}
+
+void
+PiecewiseLinearInterpolationMaterial::computeQpProperties()
+{
+  _property[_qp] = _scale_factor * _linear_interp->sample(_coupled_var[_qp]);
+  _dproperty[_qp] = _scale_factor * _linear_interp->sampleDerivative(_coupled_var[_qp]);
+}
